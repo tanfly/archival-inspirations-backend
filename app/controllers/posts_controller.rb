e4e2add51a92858197a15244f3630a883d0d9d44
tanfly <mcewen.stella@gmail.com>
2019-10-10 17:10:19 -0500

    updated controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,48 @@
 class PostsController < ApplicationController
+
+    def index 
+        user = User.find(params[:user_id])
+        posts = user.posts
+        favorites = user.favorites
+        render json: PostSerializer.new(worlds)
+    end
+
+    def show
+        post = Post.find(params[:id])
+        options = {
+            include: [:favorites]
+        }
+        render json: PostSerializer.new(post, options)
+    end
+
+    def create
+        post = Post.new(post_params)
+        if post.save
+          render json: PostSerializer.new(post)
+        end
+    end
+
+    def update
+        post = Post.find(params[:id])
+        post.update(post_params)
+        if post.save
+            render json: PostSerializer.new(post), status: :accepted
+          else
+            render json: { errors: post.errors.full_messages }, status: :unprocessible_entity
+          end
+    end
+
+    def destroy
+        post = Post.find(params[:id])
+        post.destroy
+        head 204
+      end
+
+
+    private
+
+    def world_params
+        params.require(:post).permit(:photo, :title, :description, :favorites, :time_period, :user_id)
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,50 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+
+    def index 
+        users = User.all 
+        options = {
+            include: [:posts, :favorites]
+        }
+        render json: UserSerializer.new(users, options)
+    end
+
+    def show
+        user = User.find(params[:id])
+        options = {
+            include: [:posts, :favorites]
+        }
+        render json: UserSerializer.new(user, options)
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+          render json: UserSerializer.new(user)
+        end
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        if user.save
+            render json: UserSerializer.new(user), status: :accepted
+          else
+            render json: { errors: user.errors.full_messages }, status: :unprocessible_entity
+          end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        head 204
+      end
+
+    private
+  
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :avatar, :favorites, :posts)
+    end
+        
+
+    
 end
