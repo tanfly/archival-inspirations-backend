bdbbb7c01332c6a514b3e63fc8788b8a2378ba00
tanfly <mcewen.stella@gmail.com>
2019-10-10 20:54:01 -0500

    added rack-cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    include ::ActionController::Cookies
+       def current_user
+           User.find_by(id: session[:user_id])
+           # finding the user by using the user_id in the sessions controller create action
+       end
+
+       def logged_in?
+           !!current_user
+       end
+
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,10 +8,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'localhost:3000'
-
-     resource '*',
-       headers: :any,
-       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-       credentials: true
-   end
-end
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+  end
