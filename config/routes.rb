166a11e8aee004eeb117d48ec5c8c8a46c024cd6
tanfly <mcewen.stella@gmail.com>
2019-10-10 20:38:45 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ post "/login", to: "sessions#create"
 post "/signup", to: "users#create"  
 delete "/logout", to: "sessions#destroy" 
 get "/get_current_user", to: "sessions#get_current_user"  
+
+resources :users
+resources :posts
 end
