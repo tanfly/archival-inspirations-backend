fc25ae8fcdb7572728d5d3ad3836aa8803133064
tanfly <mcewen.stella@gmail.com>
2019-10-10 20:35:12 -0500

    add ActionDispatch Cookies

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+post "/login", to: "sessions#create"
+post "/signup", to: "users#create"  
+delete "/logout", to: "sessions#destroy" 
+get "/get_current_user", to: "sessions#get_current_user"  
 end
